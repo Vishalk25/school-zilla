b56c1e856a6056e2eac80833d9419f8d7d34a8b3
Saurabh Kumar <saurabh.k150600@gmail.com>
2023-03-02 02:09:52 +0530

    replicate the figma design

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./SideBar.css";
 import schoolLogo from "../assets/schoolLogo1.png";
+import homeIcon from "../assets/pngfind.com-home-icon-png-1819775.png";
+import studentIcon from "../assets/student_education_man_bachelor_boy_-512.webp";
+import teacherIcon from "../assets/1980233-200.png";
+import eventIcon from "../assets/pngegg.png";
+import courseIcon from "../assets/12.webp";
+import userIcon from "../assets/user-128-128.png";
+import activityIcon from "../assets/activity_icon_151646.png";
 
 export default function SideBar() {
   return (
@@ -9,7 +16,36 @@ export default function SideBar() {
         <img src={schoolLogo} alt="asd" />
         <p>SchoolName1</p>
       </div>
-      <div className="menuItems"></div>
+      <div className="menuItems">
+        <div className="dashBoardButton">
+          <img src={homeIcon} alt="asd" />
+          <p>SchoolName1</p>
+        </div>
+        <div className="dashBoardButton">
+          <img src={studentIcon} alt="asd" />
+          <p>SchoolName1</p>
+        </div>
+        <div className="dashBoardButton">
+          <img src={teacherIcon} alt="asd" />
+          <p>SchoolName1</p>
+        </div>
+        <div className="dashBoardButton">
+          <img src={eventIcon} alt="asd" />
+          <p>SchoolName1</p>
+        </div>
+        <div className="dashBoardButton">
+          <img src={courseIcon} alt="asd" />
+          <p>SchoolName1</p>
+        </div>
+        <div className="dashBoardButton">
+          <img src={userIcon} alt="asd" />
+          <p>SchoolName1</p>
+        </div>
+        <div className="dashBoardButton">
+          <img src={activityIcon} alt="asd" />
+          <p>SchoolName1</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,10 +1,45 @@
 import React from "react";
+import ColorButtons from "./Button";
+import SearchInput from "./SearchInput";
 import SideBar from "./SideBar";
+import "./Student.css";
+import DataTable from "./StudentTable";
+import schoolLogo from "../assets/Screenshot (7) 4.png";
+import copyrightLogo from "../assets/@Copyright 2023, School Zilla Pvt. Ltd..png";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 export default function Student() {
   return (
-    <div>
-      <SideBar />
-    </div>
+    <>
+      <div className="mainContainer">
+        <SideBar />
+        <div className="middleContainer">
+          <div className="topHeader">
+            <h1>Students</h1>
+            <div className="profileMenu">asdasd</div>
+          </div>
+          <div className="middleHeader">
+            <SearchInput />
+            <ColorButtons />
+          </div>
+          <div className="studentDetail">
+            <DataTable />
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <img src={schoolLogo} alt="" />
+        <img src={copyrightLogo} alt="" />
+        <div className="promoContainer">
+          <LinkedInIcon />
+          <FacebookIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+        </div>
+      </div>
+    </>
   );
 }
