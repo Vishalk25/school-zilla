52d5f32c795f54a453ccda2c9c0dcb43ae82281a
Saurabh Kumar <saurabh.k150600@gmail.com>
2023-03-05 00:44:28 +0530

    Added some cosmetic changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Student from "./components/Student";
+import Student from "./components/StudentPage/Student";
 
 function App() {
   return <Student />;
--- src/components/StudentPage/Student.js
+++ src/components/StudentPage/Student.js
@@ -1,12 +1,5 @@
 import React from "react";
-import ColorButtons from "./Button";
-import SearchInput from "./SearchInput";
-import SideBar from "./SideBar";
-import "./Student.css";
 import IconButton from "@mui/material/IconButton";
-import DataTable from "./StudentTable";
-import schoolLogo from "../assets/Screenshot (7) 4.png";
-import copyrightLogo from "../assets/@Copyright 2023, School Zilla Pvt. Ltd..png";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -14,6 +7,14 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
 import AddIcon from "@mui/icons-material/Add";
+import ColorButtons from "../Button";
+import SearchInput from "../SearchInput";
+import SideBar from "../SideBar/SideBar";
+import "./Student.css";
+import DataTable from "../StudentTable";
+import schoolLogo from "../../assets/Screenshot (7) 4.png";
+// import copyrightLogo from "../../assets/Copyright-2023_-School-Zilla-Pvt.-Ltd..svg";
+import { ReactComponent as copyRightLogo } from "../../assets/Copyright-2023_-School-Zilla-Pvt.-Ltd..svg";
 
 export default function Student() {
   return (
@@ -52,7 +53,7 @@ export default function Student() {
       </div>
       <div className="footer">
         <img src={schoolLogo} alt="" />
-        <img src={copyrightLogo} alt="" />
+        <img src={copyRightLogo} alt="" />
         <div className="promoContainer">
           <LinkedInIcon />
           <FacebookIcon />
