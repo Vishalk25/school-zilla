e1ade5aa4e078363dba977fa4cd06c1b31d4fe6a
SaurabhK05 <saurabh.k150600@gmail.com>
2023-03-12 15:53:50 +0530

    Added course-page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
+// import Course from "./components/Layouts/CoursePage/Course";
 import AddNewStudent from "./components/Layouts/StudentPage/AddNewStudent";
 // import Student from "./components/Layouts/StudentPage/Student";
 import AddNewTeacher from "./components/Layouts/TeacherPage/AddNewTeacher";
 // import Teacher from "./components/Layouts/TeacherPage/Teacher";
 
 function App() {
-  // return <div>{false ? <Student /> : <Teacher />}</div>;
+  // return <div>{true ? <Student /> : <Teacher />}</div>;
   return (
     <div>
-      <AddNewStudent />
+      <AddNewTeacher />
     </div>
   );
 }
